d9a4d305b0cce0ec8baef300ae68f94e719e7755
Facundo Rios <riosfacundo7@gmail.com>
2025-04-08 09:38:54 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,13 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        Mascota miMascota = new Mascota("Buddy", "Perro", 3);
+        Libro miLibro = new Libro("Cien años de soledad", "Gabriel García Márquez", 1967);
 
-        System.out.println("Información inicial de la mascota:");
-        miMascota.mostrarInfo();
+        // Intentar modificar el año de publicación con un valor inválido
+        miLibro.setAñoPublicacion(1800);
 
-        miMascota.cumplirAnios();
+        // Intentar modificar el año de publicación con un valor válido
+        miLibro.setAñoPublicacion(2020);
 
-        System.out.println("\nInformación actualizada de la mascota:");
-        miMascota.mostrarInfo();
+        // Mostrar la información del libro
+        System.out.println("\nInformación del libro:");
+        System.out.println("Título: " + miLibro.getTitulo());
+        System.out.println("Autor: " + miLibro.getAutor());
+        System.out.println("Año de publicación: " + miLibro.getAñoPublicacion());
     }
 }
